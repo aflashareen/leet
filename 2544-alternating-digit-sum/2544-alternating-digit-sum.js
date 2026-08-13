20268a7fa2bd40e93a7f9ba0fd460aae9f04f4ec
aflashareen <aflasherin07@gmail.com>
2026-08-13 09:43:47 +0530

    Time: 0 ms (100%), Space: 53.5 MB (53.23%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var alternateDigitSum = function(n) {
+        let str = String(n)
+    let sum = 0;
+    for(let i = 0; i < str.length; i++){
+        if(i % 2 === 0){
+            sum += Number(str[i])
+        }else{
+            sum -= Number(str[i])
+        }
+    }
+        return sum
+};
